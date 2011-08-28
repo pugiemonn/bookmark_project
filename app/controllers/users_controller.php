cf6a1e41a2c00ad41a0b4a1b0e0f908e7b8d67c5
pugiemonn <pugiemonn@gmail.com>
2011-08-29 02:47:57 +0900

    ログインアクションの追加

--- app/controllers/users_controller.php
+++ app/controllers/users_controller.php
@@ -1,8 +1,21 @@
 <?php
-
 class UsersController extends AppController
 {
-    var $name = "User";
-    var $scaffold;
+  var $name = "User";
+  var $scaffold;
+  /*
+    ログイン画面を表示
+  */
+  function login()
+  {
+    
+  }
+  /*
+    ログイン処理
+  */
+  function login_cmp()
+  {
+
+  }
 }
 ?>
